5d2ec8c52c5739eeba909077860e659e0c57ad38
Crowdbotics <team@crowdbotics.com>
2020-10-19 19:23:23 +0000

    Update models

--- backend/pocketmanager_21718/wsgi.py
+++ backend/pocketmanager_21718/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pocketmanager_21718.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pocketmanager_21718.settings")
 
 application = get_wsgi_application()
